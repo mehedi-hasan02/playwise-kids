597aa4d07ba17d0933530d87df594fecd72e03f7
mehedi-hasan02 <mehedisani02@gmail.com>
2026-07-01 22:20:50 +0600

    add send invoice via an email

--- src/actions/server/order.js
+++ src/actions/server/order.js
@@ -1,37 +1,55 @@
-"use server"
+"use server";
 
 import { authOptions } from "@/lib/authOptions";
 import { collections, dbConnect } from "@/lib/dbConnect";
 import { getServerSession } from "next-auth";
 import { clearCart, getCart } from "./cart";
+import { orderInvoiceTemplate } from "@/lib/orderInvoice";
+import { sendEmail } from "@/lib/sendEmail";
 
 const orderCollection = dbConnect(collections.ORDER);
 
 export const createOrder = async (payload) => {
-    const {user} = await getServerSession(authOptions) || {}
-    if(!user){
-        return {success: false}
-    }
+  const { user } = (await getServerSession(authOptions)) || {};
+  if (!user) {
+    return { success: false };
+  }
 
-    const cart = await getCart()
+  const cart = await getCart();
 
-    if(cart.length == 0){
-        return {success : false}
-    }
+  if (cart.length == 0) {
+    return { success: false };
+  }
 
-    const newOrder = {
-        createAt: new Date().toISOString(),
-        items: cart,
-        ...payload
-    }
+  const newOrder = {
+    createAt: new Date().toISOString(),
+    items: cart,
+    ...payload,
+  };
 
-    const result = await orderCollection.insertOne(newOrder)
+  const totalPrice = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0,
+  );
 
-    if(Boolean(result.insertedId)){
-        const result = await clearCart()
-    }
+  const result = await orderCollection.insertOne(newOrder);
 
-    return{
-        success: result.insertedId
-    }
-}
+  if (Boolean(result.insertedId)) {
+    const result = await clearCart();
+  }
+
+  // 📧 Send Invoice Email
+  await sendEmail({
+    to: user?.email,
+    subject: "Your Order Invoice - PlayWise Kids",
+    html: orderInvoiceTemplate({
+      orderId: result?.insertedId.toString(),
+      items: cart,
+      totalPrice,
+    }),
+  });
+
+  return {
+    success: result.insertedId,
+  };
+};
